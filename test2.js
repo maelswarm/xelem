514700c475796372480d615d0c3f856475249601
Brandon Barber <brandonbarber@Brandons-MacBook-Pro.local>
2018-05-04 11:38:35 -0500

    minor

--- test2.js
+++ test2.js
@@ -1,18 +1,18 @@
-let f = 'this should be before';
+let xelem1000 = document.createElement('div');xelem1000.setAttribute('class',c);xelem1000.setAttribute('id','world');xelem1000.innerHTML = `<div class="${c}" id="${d}">
+      Hello Universe
+    </div>`;let f = 'this should be before';
 let c = 'hello';
-let d = 'universe';let xelem1000 = document.createElement('div');xelem1000.setAttribute('class',c);xelem1000.setAttribute('id','world');xelem1000.innerHTML = `<div class="${c}" id="${d}">
+let d = 'universe';
+document.querySelector('.st0').appendChild(xelem1000);let xelem1001 = document.createElement('div');xelem1001.setAttribute('class',c);xelem1001.setAttribute('id','world');xelem1001.innerHTML = `<div class="${c}" id="${d}">
       Hello Universe
     </div>`;
-document.querySelector('.st0').appendChild(xelem1000);
 
 c = 'hello';
-d = 'universe';let xelem1001 = document.createElement('div');xelem1001.setAttribute('class',c);xelem1001.setAttribute('id','world');xelem1001.innerHTML = `<div class="${c}" id="${d}">
+d = 'universe';
+document.querySelector('.st0').appendChild(xelem1001);let xelem1002 = document.createElement('div');xelem1002.setAttribute('class',c);xelem1002.setAttribute('id','world');xelem1002.innerHTML = `<div class="${c}" id="${d}">
       Hello Universe
     </div>`;
-document.querySelector('.st0').appendChild(xelem1001);
 
 c = 'hello';
-d = 'universe';let xelem1002 = document.createElement('div');xelem1002.setAttribute('class',c);xelem1002.setAttribute('id','world');xelem1002.innerHTML = `<div class="${c}" id="${d}">
-      Hello Universe
-    </div>`;
+d = 'universe';
 document.querySelector('.st0').appendChild(xelem1002);
